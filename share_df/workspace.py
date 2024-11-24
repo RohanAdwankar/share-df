0e111bfa0c9192fb190868dd6fc1c5f9ad7baeb8
Rohan Adwankar <39285979+RohanAdwankar@users.noreply.github.com>
2024-11-23 17:06:56 -0800

    swap from ngrok cli to sdk

--- share_df/workspace.py
+++ share_df/workspace.py
@@ -1,19 +1,21 @@
 from share_df import create_share_server
 import time
 import os
+import ngrok
 
 def run_server():
     content = "<h2>Place Holder HTML</h2><p>This website could then be used for displaying the GUI!</p>"
-    url = create_share_server(
+    return create_share_server(
         content=content,
         title="My Shared Content",
         duration_hours=1
     )
-    print(f"Access your content at: {url}")
 
-def run_ngrok(email):
-    os.system(f"ngrok http http://localhost:8000 --oauth google --oauth-allow-email {email}")
+def run_ngrok(url, email):
+    listener = ngrok.forward(url, authtoken_from_env=True, oauth_provider="google", oauth_allow_emails=email)
+    print(f"Ingress established at: {listener.url()}");
+    time.sleep(10)
 
-run_server()
-email = input("Which gmail do you want to share this with?")
-run_ngrok(email=email)
+url = run_server()
+email = input("Which gmail do you want to share this with? ")
+run_ngrok(url=url, email=email)
