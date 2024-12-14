3502057927a1c9eb7050ba3968f8b1eadc97ba09
Rohan Adwankar <39285979+RohanAdwankar@users.noreply.github.com>
2024-12-14 00:20:37 -0800

    1.0.7 update docstring, function signatures

--- share_df/__init__.py
+++ share_df/__init__.py
@@ -8,19 +8,20 @@ def pandaBear(df: pd.DataFrame, use_iframe: bool = False) -> pd.DataFrame:
     Opens an interactive web editor for a pandas DataFrame with authentication.
     
     Args:
-        df (pd.DataFrame): The DataFrame to edit
+        df (pd.DataFrame): The DataFrame to edit.
+        use_iframe (bool, optional): Whether to display the editor in an iframe (Google Colab only). Defaults to False.
         
     Returns:
-        pd.DataFrame: The edited DataFrame
+        pd.DataFrame: The edited DataFrame.
     """
     from .server import start_editor
-    return start_editor(df, use_iframe)
+    return start_editor(df, use_iframe=use_iframe)
 
 @pd.api.extensions.register_dataframe_accessor("pandaBear")
 class PandaBearAccessor:
     def __init__(self, pandas_obj):
         self._obj = pandas_obj
         
-    def __call__(self):
-        self._obj.update(pandaBear(self._obj))
+    def __call__(self, use_iframe: bool = False):
+        self._obj.update(pandaBear(self._obj, use_iframe=use_iframe))
         return None
